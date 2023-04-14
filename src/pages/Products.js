25c5adaca881265f0bb0a2c4fca10e6d2fd70262
SauBanh <saubanh05062001@gmail.com>
2023-04-14 17:53:16 +0700

    Pages with Links

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Products() {
-    return <div style={{ color: '#fff' }}>Products</div>;
+    return (
+        <div>
+            <div>Products</div>
+            go to<Link to="/">Page Home</Link>
+        </div>
+    );
 }
 
 export default Products;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function home() {
-    return <div>home</div>;
+    return (
+        <div>
+            <div>Home</div>
+            go to<Link to="/products">Page Products</Link>
+        </div>
+    );
 }
 
 export default home;

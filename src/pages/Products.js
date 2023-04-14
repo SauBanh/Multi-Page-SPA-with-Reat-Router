f1e220ac07c0735ae80a364180515a4c0c5ab2bc
SauBanh <saubanh05062001@gmail.com>
2023-04-15 02:21:44 +0700

    Link for Dynamic

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const PRODUCT = [
+    { id: 'p1', title: 'Product 1' },
+    { id: 'p2', title: 'Product 2' },
+    { id: 'p3', title: 'Product 3' },
+];
+
 function Products() {
     return (
         <div>
             <div>Products</div>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                {PRODUCT.map((product) => (
+                    <li key={product.id}>
+                        <Link to={`/products/${product.id}`}>{product.title}</Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
